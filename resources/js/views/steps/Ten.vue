1c419c0671330277da2f4cd829a8a77cd1917e31
Josue <sigue.nox@gmail.com>
2020-10-28 10:28:46 +0100

    add link to main site

--- resources/js/views/steps/Ten.vue
+++ resources/js/views/steps/Ten.vue
@@ -1,24 +1,30 @@
 <template>
-  <step title="OPERAZIONE CONCLUSA" number="10">
-    <v-row>
-      <v-col cols="12" class="d-flex justify-center align-center">
-        <v-icon color="success" size="150">mdi-check</v-icon>
-      </v-col>
-      <v-col cols="12" class="d-flex align-center">
-        <p class="text-center mx-auto">
-          Complimenti! Il processo di autoidentificazione è stato effettuato con
-          successo. A breve riceverai una e-mail di conferma.
-        </p>
-      </v-col>
-    </v-row>
-  </step>
+    <step title="OPERAZIONE CONCLUSA" number="10">
+        <v-row>
+            <v-col cols="12" class="d-flex justify-center align-center">
+                <v-icon color="success" size="150">mdi-check</v-icon>
+            </v-col>
+            <v-col cols="12" class="d-flex align-center">
+                <p class="text-center mx-auto">
+                    Complimenti! Il processo di autoidentificazione è stato
+                    effettuato con successo. A breve riceverai una e-mail di
+                    conferma.
+                </p>
+            </v-col>
+            <v-col class="text-center">
+                <v-btn href="https://online.betitaly.it/" color="success"
+                    >Vai a BetItaly.it</v-btn
+                >
+            </v-col>
+        </v-row>
+    </step>
 </template>
 
 <script>
 import Step from "../../components/Step";
 export default {
-  components: {
-    Step,
-  },
+    components: {
+        Step
+    }
 };
 </script>
